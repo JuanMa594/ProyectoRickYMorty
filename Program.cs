18e49f53cce4b54f033cedd015d71f51e3fd4d41
JUAN MANUEL USUGA GALEANO <juanusuga1113848@correo.itm.edu.co>
2026-01-29 20:59:41 -0500

    Se cambió el modo de renderizado al InteractiveServer, para que no generara conflictos a la hora de tratar de ejecutar el proyecto

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using ProyectoRickYMorty.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -16,19 +15,18 @@ builder.Services.AddHttpClient("RickAndMortyApi", client =>
 builder.Services.AddScoped<IRickMortyService, RickMortyService>();
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
--- obj/Debug/net10.0/ProyectoRickYMorty.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoRickYMorty.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoRickYMorty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae66ce1b12e7ba618806b00a7cf7bdb6de25697e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc649e15af986150c3d934ed4efbba970a9e0b77")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoRickYMorty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoRickYMorty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
